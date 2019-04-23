f70d2449d29c2c02e1bd8753fb7792925bc4ac39
188759 <houxiaozhuang@ncarzone.com>
2019-04-23 21:58:06 +0800

    项目搭建完成，完成启动

--- biz/mitutucue-service/src/main/java/com/xiaozhuanglt/mitutucue/service/UserDao.java
+++ biz/mitutucue-service/src/main/java/com/xiaozhuanglt/mitutucue/service/UserDao.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Component
-@Controller
-@Transactional
+
 public class UserDao {
 
 }
