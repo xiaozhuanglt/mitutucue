ab51671e09a218645f1b98f8e4b2759da4a234e1
hxz <hou.xiaozhuang@tngdigital.com.my>
2023-02-16 01:05:33 +0800

    日志模板优化

--- biz/mitutucue-service/src/main/java/com/xiaozhuanglt/mitutucue/service/facadeimpl/DemoFacadeImpl.java
+++ biz/mitutucue-service/src/main/java/com/xiaozhuanglt/mitutucue/service/facadeimpl/DemoFacadeImpl.java
@@ -4,6 +4,7 @@ import com.xiaozhuanglt.mitutucue.facade.DemoFacade;
 import com.xiaozhuanglt.mitutucue.service.interfaces.ServiceDemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -20,8 +21,10 @@ public class DemoFacadeImpl implements DemoFacade {
     @Override
     public int queryArea(Long areaId) {
         try {
+            System.out.println("=========1:"+ MDC.get("traceId"));
             logger.info("收到queryArea 的查询请求，areaId:"+areaId);
             int i = serviceDemo.queryArea(areaId);
+            System.out.println("=========2:"+MDC.get("traceId"));
             return i;
         } catch (Exception e) {
             logger.error("查询queryArea错误：",e);
--- web/mitutucue-web/src/main/java/com/xiaozhuanglt/mitutucue/config/TraceIdFilter.java
+++ web/mitutucue-web/src/main/java/com/xiaozhuanglt/mitutucue/config/TraceIdFilter.java
@@ -8,6 +8,7 @@ import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.*;
 import org.slf4j.MDC;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
 
@@ -22,22 +23,21 @@ public class TraceIdFilter implements Filter {
         RpcServiceContext rpcServiceContext = RpcContext.getServiceContext();
 
         String traceId;
-        if (rpcServiceContext.isConsumerSide()) {
-            traceId = rpcServiceContext.getAttachment("traceId");
-            if (traceId == null) {
-                traceId = UUID.randomUUID().toString();
-                rpcServiceContext.setAttachment("traceId",traceId);
-            }
-            MDC.put("traceId", traceId);
-
+        if (null == rpcServiceContext.getUrl()){
+            return invoker.invoke(invocation);
         }
-        if (rpcServiceContext.isProviderSide()) {
+        if (StringUtils.isEmpty(MDC.get("traceId"))){
             traceId = rpcServiceContext.getAttachment("traceId");
-            if (traceId == null) {
+            if (traceId == null){
                 traceId = UUID.randomUUID().toString();
                 rpcServiceContext.setAttachment("traceId",traceId);
             }
             MDC.put("traceId", traceId);
+        }else {
+            traceId = rpcServiceContext.getAttachment("traceId");
+            if (traceId == null){
+                rpcServiceContext.setAttachment("traceId",MDC.get("traceId"));
+            }
         }
         return invoker.invoke(invocation);
     }
